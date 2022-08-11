61542673a0b6d33ee669fc0ff2cfd975fe22d4e2
Andrew Faugno <andrewfaugno825@gmail.com>
2022-08-11 17:25:35 -0400

    created modal when images are clicked

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function Modal() {
+function Modal({currentPhoto}) {
+    const {name, category, description, index} = currentPhoto;
 
     return(
-        // jsx
+        <div className='modalBackdrop'>
+            <div className='modalContainer'>
+                <h3 className='modalTitle'>{name}</h3>
+                <img 
+                    src={require(`../../assets/large/${category}/${index}.jpg`)}
+                    alt={category}
+                />
+                <p>{description}</p>
+                <button type='button'>
+                    Close this modal
+                </button>
+            </div>
+        </div>
     );
 }
 
